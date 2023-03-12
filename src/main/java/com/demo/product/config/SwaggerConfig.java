c24a59048ba5b601eee9c6909663166311637f71
Tabish Matin <tabishmatin1@gmail.com>
2023-03-12 23:43:56 +0530

    Refactoring

---
+++ src/main/java/com/demo/product/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.demo.product.config;
+
+public class SwaggerConfig {
+}
--- src/main/java/com/demo/product/controller/DemoProductController.java
+++ src/main/java/com/demo/product/controller/DemoProductController.java
@@ -1,4 +1,4 @@
-package com.demo.product;
+package com.demo.product.controller;
 
 import io.micronaut.http.annotation.*;
 
