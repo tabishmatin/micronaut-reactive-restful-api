4e8df290a45c48b498d89cca21b0a5007cd4bcb4
Tabish Matin <tabishmatin1@gmail.com>
2023-03-15 00:21:01 +0530

    Added: Service layer for Product CRUD operations

--- src/main/java/com/demo/product/service/ProductService.java
+++ src/main/java/com/demo/product/service/ProductService.java
@@ -1,4 +1,17 @@
 package com.demo.product.service;
 
+import com.demo.product.dto.ProductRequestDto;
+import com.demo.product.dto.ProductResponseDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ProductService {
+
+    Mono<ProductResponseDto> addProduct(ProductRequestDto productRequestDto);
+    Mono<ProductResponseDto> getProductById(String productId);
+    Mono<ProductResponseDto> getProductByName(String productName);
+    Flux<ProductResponseDto> getAllProducts();
+    Mono<ProductResponseDto> updateProductById(String productId);
+    Mono<String> deleteProductById(String productId);
+
 }
--- src/main/java/com/demo/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/demo/product/service/impl/ProductServiceImpl.java
@@ -1,8 +1,42 @@
 package com.demo.product.service.impl;
 
+import com.demo.product.dto.ProductRequestDto;
+import com.demo.product.dto.ProductResponseDto;
 import com.demo.product.service.ProductService;
 import jakarta.inject.Singleton;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Singleton
 public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public Mono<ProductResponseDto> addProduct(ProductRequestDto productRequestDto) {
+        return null;
+    }
+
+    @Override
+    public Mono<ProductResponseDto> getProductById(String productId) {
+        return null;
+    }
+
+    @Override
+    public Mono<ProductResponseDto> getProductByName(String productName) {
+        return null;
+    }
+
+    @Override
+    public Flux<ProductResponseDto> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Mono<ProductResponseDto> updateProductById(String productId) {
+        return null;
+    }
+
+    @Override
+    public Mono<String> deleteProductById(String productId) {
+        return null;
+    }
 }
