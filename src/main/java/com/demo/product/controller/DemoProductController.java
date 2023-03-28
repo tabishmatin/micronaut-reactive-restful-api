c3649010f9606bae30c34afb9fcdbef690ac87d3
Tabish Matin <tabishmatin1@gmail.com>
2023-03-28 23:34:45 +0530

    Feat: Added getProductById API

--- src/main/java/com/demo/product/controller/DemoProductController.java
+++ src/main/java/com/demo/product/controller/DemoProductController.java
@@ -1,13 +1,11 @@
 package com.demo.product.controller;
 
 import com.demo.product.dto.ProductRequestDto;
+import com.demo.product.dto.ProductResponseDto;
 import com.demo.product.service.ProductService;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpStatus;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Status;
+import io.micronaut.http.annotation.*;
 import jakarta.inject.Inject;
 import reactor.core.publisher.Mono;
 
@@ -25,5 +23,11 @@ public class DemoProductController {
     @Status(HttpStatus.CREATED)
     Mono<Object> addProduct(@NonNull @Body ProductRequestDto dto) {
         return productService.addProduct(dto);
+    }
+
+    @Get("/{product-id}")
+    @Status(HttpStatus.ACCEPTED)
+    Mono<ProductResponseDto> getProductById(@NonNull @PathVariable("product-id") String productId) {
+        return productService.getProductById(productId);
     }
 }
