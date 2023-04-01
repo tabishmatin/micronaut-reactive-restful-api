e435ee66357484b08122a758ed8c70f133546c9e
Tabish Matin <tabishmatin1@gmail.com>
2023-04-01 21:36:06 +0530

    Added Utility package -> Constants

---
+++ src/main/java/com/demo/product/util/Constants.java
@@ -0,0 +1,10 @@
+package com.demo.product.util;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String PRODUCT_ALREADY_EXISTS = "Product with the same name already exists";
+    public static final String PRODUCT_NOT_FOUND = "Product with the given ID not found";
+}
